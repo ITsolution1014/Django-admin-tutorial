59b326bfc99453f414d52538fce3017523fca66d
ITsolution1014 <itsolution1014@gmail.com>
2021-07-28 23:34:58 +0300

    Remove booking

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,24 +2,4 @@ from django.db import models
 
 import uuid # Required for unique book instances
 
-class Book(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
-    title = models.CharField(max_length=200)
-
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
-
-    def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
 
